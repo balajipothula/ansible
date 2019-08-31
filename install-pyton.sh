800343faea92555f4457a751e83e76e923f3e549
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-31 16:14:46 +0530

    Create install-pyton.sh

---
+++ install-pyton.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 31 August 2019,
+# Description : Installing / Compiling Python from Source Code.
+
+# update package repositories.
+sudo yum -y update
+
+# installing build essentials.
+sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
+
+# python downloading, extracting and compiling.
+curl -JL https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz -o Python.tgz && \
+tar -xzf $HOME/Python.tgz -C $HOME                                              && \
+rm  -rf  $HOME/Python.tar.gz                                                    && \
+mv       $HOME/Python-* Python                                                  && \
+mkdir -p $HOME/python                                                           && \
+cd       $HOME/Python                                                           && \
+./configure --enable-optimizations --prefix=$HOME/python                        && \
+make altinstall                                                                 && \
+cd       $HOME                                                                  && \
+rm  -rf  $HOME/Python                                                           && \
+ln       $HOME/python/bin/python3.7 $HOME/python/bin/python                     && \
+ln       $HOME/python/bin/pip3.7    $HOME/python/bin/pip                        && \
+echo "export PATH=$PATH:$HOME/Python/bin" >> $HOME/.bashrc                      && \
+exec $BASH
