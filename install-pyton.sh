7d84558aca767fd600405ed226399d127226706a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-31 16:49:27 +0530

    Update install-pyton.sh

--- install-pyton.sh
+++ install-pyton.sh
@@ -11,17 +11,17 @@ sudo yum -y update
 sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
 
 # python downloading, extracting and compiling.
-curl -JL https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz -o Python.tgz && \
-tar -xzf $HOME/Python.tgz -C $HOME                                              && \
-rm  -rf  $HOME/Python.tgz                                                       && \
-mv       $HOME/Python-* Python                                                  && \
-mkdir -p $HOME/python                                                           && \
-cd       $HOME/Python                                                           && \
-./configure --enable-optimizations --prefix=$HOME/python                        && \
-make altinstall                                                                 && \
-cd       $HOME                                                                  && \
-rm  -rf  $HOME/Python                                                           && \
-ln       $HOME/python/bin/python3.7 $HOME/python/bin/python                     && \
-ln       $HOME/python/bin/pip3.7    $HOME/python/bin/pip                        && \
-echo "export PATH=$PATH:$HOME/Python/bin" >> $HOME/.bashrc                      && \
+curl -JL https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz -o $HOME/Python.tgz && \
+tar -xzf $HOME/Python.tgz -C $HOME                                                    && \
+rm  -rf  $HOME/Python.tgz                                                             && \
+mv       $HOME/Python-* Python                                                        && \
+mkdir -p $HOME/python                                                                 && \
+cd       $HOME/Python                                                                 && \
+./configure --enable-optimizations --prefix=$HOME/python                              && \
+make altinstall                                                                       && \
+cd       $HOME                                                                        && \
+rm  -rf  $HOME/Python                                                                 && \
+ln       $HOME/python/bin/python3.7 $HOME/python/bin/python                           && \
+ln       $HOME/python/bin/pip3.7    $HOME/python/bin/pip                              && \
+echo "export PATH=$PATH:$HOME/Python/bin" >> $HOME/.bashrc                            && \
 exec $BASH
